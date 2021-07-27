e517dd024698e4c7903266c7fafab8776d40b0db
Youngbeom Choi <helloybz@gmail.com>
2021-07-27 17:20:37 +0900

    Add a MUSDB18 wrapper with torch Dataset
    - Add test codes for the dataset.
    - Make use of 'musdb' package which is provided by sigsep.
    - Add 'torch' and 'musdb' to requirements.txt.

---
+++ musicsplitter/datasets.py
@@ -0,0 +1,42 @@
+import random
+
+import torch
+import musdb
+
+
+class MUSDB18(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        data_root,
+        download=False,
+        subsets=['train', 'test'],
+        chunk_duration_in_sec=5,
+        sample_rate=44100
+    ):
+        super(MUSDB18, self).__init__()
+        self.mus = musdb.DB(
+            root=data_root,
+            download=download,
+            subsets=subsets,
+            sample_rate=sample_rate
+        )
+        self.chunk_duration_in_sec = chunk_duration_in_sec
+        self.sample_rate = sample_rate
+
+    def __getitem__(self, idx):
+        track = self.mus[idx]
+
+        # make random crop
+        track.chunk_duration = self.chunk_duration_in_sec
+        track.chunk_start = random.uniform(0, track.duration - self.chunk_duration_in_sec)
+
+        audio = track.audio.T
+        vocal = track.targets['vocals'].audio.T
+        drum = track.targets['drums'].audio.T
+        bass = track.targets['bass'].audio.T
+        other = track.targets['other'].audio.T
+
+        return audio, vocal, drum, bass, other
+
+    def __len__(self):
+        return len(self.mus.tracks)
---
+++ tests/test_dataset.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+import unittest
+
+from musicsplitter.datasets import MUSDB18
+
+
+class TestMUSDB18(unittest.TestCase):
+    def setUp(self):
+        data_root = Path('./tests/data_sample').absolute()
+        assert data_root.exists(), data_root
+
+        self.mus_train = MUSDB18(
+            data_root=data_root,
+            download=False,
+            subsets='train',
+            chunk_duration_in_sec=10,
+            sample_rate=22050,
+        )
+
+    def test_length(self):
+        assert len(self.mus_train) == 1
+
+    def test_getitem(self):
+        audio, vocal, drum, bass, other = self.mus_train[0]
+
+        assert audio.shape[0] == 2  # stereo
+        assert audio.shape[1] == self.mus_train.chunk_duration_in_sec * self.mus_train.sample_rate
+
+        assert audio.shape == vocal.shape == drum.shape == bass.shape == other.shape
