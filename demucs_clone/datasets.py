33be70178935c100ee6c0baa60e26f256e53183f
Youngbeom Choi <helloybz@gmail.com>
2021-08-02 21:04:27 +0900

    Update MUSDB18 getitem
     - MUSDB18 takes argument 'sources'
     - MUSDB18 __getitem__ method returns audio and targets tensor.
     - targets tensor has shape (num_sources, num_channel, T)

--- demucs_clone/datasets.py
+++ demucs_clone/datasets.py
@@ -1,5 +1,5 @@
 import random
-from typing import Literal
+from typing import Literal, Sequence
 
 import torch
 import musdb
@@ -11,6 +11,7 @@ class MUSDB18(torch.utils.data.Dataset):
         data_root,
         download:       bool = False,
         split:          Literal['train', 'valid', 'test'] = 'train',
+        sources:        Sequence[str] = ['drums', 'bass', 'vocals', 'other'],
         chunk_duration: int = 5,
         sample_rate:    int = 44100,
     ) -> None:
@@ -35,8 +36,10 @@ class MUSDB18(torch.utils.data.Dataset):
         else:
             raise ValueError
 
+        sources.sort()
         self.chunk_duration = chunk_duration
         self.sample_rate = sample_rate
+        self.sources = sources
 
     def __getitem__(self, idx):
         track = self.mus[idx]
@@ -45,19 +48,15 @@ class MUSDB18(torch.utils.data.Dataset):
         track.chunk_duration = self.chunk_duration
         track.chunk_start = random.uniform(0, track.duration - self.chunk_duration)
 
-        audio = track.audio.T
-        vocal = track.targets['vocals'].audio.T
-        drum = track.targets['drums'].audio.T
-        bass = track.targets['bass'].audio.T
-        other = track.targets['other'].audio.T
+        audio = torch.from_numpy(track.audio.T).float()
 
-        audio = torch.from_numpy(audio).float()
-        vocal = torch.from_numpy(vocal).float()
-        drum = torch.from_numpy(drum).float()
-        bass = torch.from_numpy(bass).float()
-        other = torch.from_numpy(other).float()
+        targets = []
+        for source in self.sources:
+            target_source_audio = torch.from_numpy(track.targets[source].audio.T).float()
+            targets.append(target_source_audio)
+        targets = torch.stack(targets)
 
-        return audio, vocal, drum, bass, other
+        return audio, targets
 
     def __len__(self):
         return len(self.mus.tracks)
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -8,11 +8,11 @@ from demucs_clone.datasets import MUSDB18
 
 class TestMUSDB18(unittest.TestCase):
     def setUp(self):
-        data_root = Path('./tests/data_sample').absolute()
-        assert data_root.exists(), data_root
+        self.data_root = Path('./tests/data_sample').absolute()
+        assert self.data_root.exists(), self.data_root
 
         self.mus_train = MUSDB18(
-            data_root=data_root,
+            data_root=self.data_root,
             download=False,
             split='train',
             chunk_duration=5,
@@ -20,7 +20,7 @@ class TestMUSDB18(unittest.TestCase):
         )
 
         self.mus_valid = MUSDB18(
-            data_root=data_root,
+            data_root=self.data_root,
             download=False,
             split='valid',
             chunk_duration=5,
@@ -28,7 +28,7 @@ class TestMUSDB18(unittest.TestCase):
         )
 
         self.mus_test = MUSDB18(
-            data_root=data_root,
+            data_root=self.data_root,
             download=False,
             split='test',
             chunk_duration=5,
@@ -46,15 +46,41 @@ class TestMUSDB18(unittest.TestCase):
         assert len(self.mus_test) == 1
 
     def test_getitem(self):
-        audio, vocal, drum, bass, other = self.mus_train[0]
+        audio, targets = self.mus_train[0]
 
         assert audio.shape[0] == 2  # stereo
         assert audio.shape[1] == self.mus_train.chunk_duration * self.mus_train.sample_rate
 
-        assert audio.shape == vocal.shape == drum.shape == bass.shape == other.shape
+        assert audio.shape[-1] == targets.shape[-1]
 
     def test_dtype(self):
         data = self.mus_train[0]
 
         for data_ in data:
             assert data_.dtype == torch.float32
+
+    def test_select_sources_from_dataset(self):
+        C = 2
+        sources = ['drums', 'bass']
+        mus_drum_bass = MUSDB18(
+            data_root=self.data_root,
+            download=False,
+            split='train',
+            sources=sources,
+            chunk_duration=5,
+            sample_rate=44100,
+        )
+
+        assert mus_drum_bass[0][1].shape == (len(sources), C, 44100*5)
+
+        sources = ['drums', 'bass', 'vocals', 'other']
+        mus_drum_bass = MUSDB18(
+            data_root=self.data_root,
+            download=False,
+            split='train',
+            sources=sources,
+            chunk_duration=5,
+            sample_rate=44100,
+        )
+
+        assert mus_drum_bass[0][1].shape == (len(sources), C, 44100*5)
