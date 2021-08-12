608ff2919b1fa890fe52fc0446dcfc6d257b013e
Youngbeom Choi <helloybz@gmail.com>
2021-08-12 12:22:36 +0900

    Edit validation set definition

--- demucs_clone/datasets.py
+++ demucs_clone/datasets.py
@@ -42,26 +42,37 @@ class MUSDB18(torch.utils.data.Dataset):
         self.chunk_duration = chunk_duration
         self.sample_rate = sample_rate
         self.sources = sources
-
+        self.split = split
         self._segment_map = [int(track.duration) - self.chunk_duration + 1 for track in self.mus]
 
     def __getitem__(self, idx):
 
-        cursor = 0
-        for i, segment_size in enumerate(self._segment_map):
-            next_cursor_position = cursor + segment_size
-            if next_cursor_position > idx:
-                track_idx = i
-                offset = idx - cursor
-                break
-            else:
-                cursor = next_cursor_position
+        if self.split == 'train':
+            cursor = 0
+            for i, segment_size in enumerate(self._segment_map):
+                next_cursor_position = cursor + segment_size
+                if next_cursor_position > idx:
+                    track_idx = i
+                    offset = idx - cursor
+                    break
+                else:
+                    cursor = next_cursor_position
+        elif self.split in ['valid', 'test']:
+            track_idx = idx
+        else:
+            raise ValueError
 
         track = self.mus[track_idx]
 
-        # make random crop
-        track.chunk_duration = self.chunk_duration - 1
-        track.chunk_start = offset + random.uniform(0, 1)
+        if self.split == 'train':
+            # make random crop
+            track.chunk_duration = self.chunk_duration - 1
+            track.chunk_start = offset + random.uniform(0, 1)
+        elif self.split == 'valid':
+            track.chunk_duration = self.chunk_duration
+            track.chunk_start = 0
+        else:
+            raise ValueError
 
         sources = []
         for source in self.sources:
@@ -70,30 +81,38 @@ class MUSDB18(torch.utils.data.Dataset):
 
         # Data Augumentation
         # Pitch shift
-        if torch.bernoulli(torch.Tensor([0.2])):
-            effects = [
-                # convert cents into semitones by multiplying 100
-                ['pitch', f'{random.randint(-2, 2)*100}'],
-                ['rate', f'{self.sample_rate}'],
-                ['stretch', f'{random.randint(88, 112)*0.01}'],
-                ['rate', f'{self.sample_rate}'],
-            ]
+        if self.split == 'train':
+            if torch.bernoulli(torch.Tensor([0.2])):
+                effects = [
+                    # convert cents into semitones by multiplying 100
+                    ['pitch', f'{random.randint(-2, 2)*100}'],
+                    ['rate', f'{self.sample_rate}'],
+                    ['stretch', f'{random.randint(88, 112)*0.01}'],
+                    ['rate', f'{self.sample_rate}'],
+                ]
 
-            # TODO: replace with torchaudio.functional.pitch_shift
-            for i, source in enumerate(sources):
-                source, _ = torchaudio.sox_effects.apply_effects_tensor(
-                    tensor=source,
-                    sample_rate=self.sample_rate,
-                    effects=effects,
-                )
-                sources[i] = source
+                # TODO: replace with torchaudio.functional.pitch_shift
+                for i, source in enumerate(sources):
+                    source, _ = torchaudio.sox_effects.apply_effects_tensor(
+                        tensor=source,
+                        sample_rate=self.sample_rate,
+                        effects=effects,
+                    )
+                    sources[i] = source
 
         sources = torch.stack(sources)
 
         return sources
 
     def __len__(self):
-        return sum(self._segment_map)
+        if self.split == 'train':
+            return sum(self._segment_map)
+        elif self.split == 'valid':
+            return len(self.mus.tracks)
+        elif self.split == 'test':
+            pass
+        else:
+            raise ValueError
 
 
 def collate_shortest(batch_sources):
