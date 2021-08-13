35a74b47cf27d67bd5e1d6de5e017c3fe0fc09bd
Youngbeom Choi <helloybz@gmail.com>
2021-08-13 10:43:25 +0900

    DDP for training

--- demucs_clone/workers.py
+++ demucs_clone/workers.py
@@ -4,6 +4,7 @@ from numpy import Inf
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from torch.utils.data.distributed import DistributedSampler
 
 from .datasets import collate_shortest
 
@@ -17,23 +18,31 @@ class Worker:
         batch_size: int,
         num_workers: int,
         device,
+        world_size,
     ) -> None:
         self.model = model
         self.dataset = dataset
         self.criterion = criterion
-        self.batch_size = batch_size
         self.num_workers = num_workers
         self.device = device
+        self.world_size = world_size
+        self.batch_size = batch_size // world_size
+
+        if world_size > 1:
+            self.sampler = DistributedSampler(
+                dataset=dataset,
+            )
 
     def _init_dataloader(self):
         return DataLoader(
             dataset=self.dataset,
             batch_size=self.batch_size if self.dataset.split == 'train' else 1,
-            shuffle=self.dataset.split == 'train',
+            shuffle=self.dataset.split == 'train' and (self.sampler is None),
             num_workers=self.num_workers,
             pin_memory=True,
             drop_last=self.dataset.split != 'train',
             collate_fn=collate_shortest,
+            sampler=self.sampler if self.world_size > 1 else None,
         )
 
 
@@ -48,20 +57,24 @@ class Trainer(Worker):
         batch_size,
         num_workers,
         device,
+        world_size,
     ) -> None:
 
-        super(Trainer, self).__init__(model, dataset, criterion, batch_size, num_workers, device)
+        super(Trainer, self).__init__(model, dataset, criterion, batch_size, num_workers, device, world_size)
 
         self.optimizer = optimizer
         self.augmentations = nn.Sequential(*augmentations)
         self.augmentations.to(self.device)
 
-    def train(self) -> None:
+    def train(self, epoch) -> None:
         '''
         Train the model with dataset for 1 epoch.
         '''
         self.model.train()
         dataloader = self._init_dataloader()
+
+        if self.world_size > 1:
+            self.sampler.set_epoch(epoch)
 
         for y in dataloader:
             y = y.to(self.device, non_blocking=True)
@@ -74,6 +87,8 @@ class Trainer(Worker):
             cost = self.criterion(input=y, target=y_hat)
             cost.backward()
             self.optimizer.step()
+
+            del x, y, y_hat
 
             yield cost.item()
 
@@ -96,8 +111,9 @@ class Validator(Worker):
         num_workers:        int,
         validation_period:  int,
         device,
+        world_size,
     ) -> None:
-        super(Validator, self).__init__(model, dataset, criterion, batch_size, num_workers, device)
+        super(Validator, self).__init__(model, dataset, criterion, batch_size, num_workers, device, world_size)
 
         self.validation_period = validation_period
         self.loss_best = Inf
